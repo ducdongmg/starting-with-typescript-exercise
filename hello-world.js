88fa7e35dc3ea37e522a502a0033e748d1beb09b
Nguyen Duc Dong <nddong@rakus.co.jp>
2020-07-29 14:56:44 +0900

    from vscode

--- hello-world.js
+++ hello-world.js
@@ -2,3 +2,5 @@ function doIt() {
     var theText = "Hello World";
     alert(theText);
 }
+var message = 'Hello World';
+console.log(message);
--- hello-world.ts
+++ hello-world.ts
@@ -2,4 +2,8 @@ function doIt(): void {
     const theText: string = "Hello World";
   
     alert(theText);
-  }
+}
+
+let message: string = 'Hello World';
+console.log(message);
+console.warn(message);
